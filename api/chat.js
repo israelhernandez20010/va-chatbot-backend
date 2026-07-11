100cf1bbb9de088d2a3c0f566068fd76ceabf0e6
israelhernandez20010 <israelhernandez20010@gmail.com>
2026-07-11 11:19:16 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "gpt-5.6-luna",
+          model: "gpt-5.4-nano",
           messages,
           temperature: 0.3
         })
